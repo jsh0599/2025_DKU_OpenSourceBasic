5a3afe2c54843fe17baa9ff3bc3d121dd80b2433
jsh0599 <wkdtlsgur0599@naver.com>
2025-06-02 15:59:05 +0900

    Update contentmenu.hpp

--- contentmenu.hpp
+++ contentmenu.hpp
@@ -1,1 +1,13 @@
+#ifndef CONTENTMENU_HPP
+#define CONTENTMENU_HPP
 
+#include "menustate.hpp"
+
+class ContentMenu : public MenuState {
+public:
+    virtual ~ContentMenu() {}
+    // 공통 메뉴 기능 예시
+    virtual void handleSelection(int optionIndex) = 0;
+};
+
+#endif
