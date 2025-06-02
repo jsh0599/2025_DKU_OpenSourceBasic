d115b4a82caa3adcffb05fe4417453e2844dddc5
jsh0599 <wkdtlsgur0599@naver.com>
2025-06-02 14:01:45 +0900

    Create menustateEx.hpp

---
+++ menustateEx.hpp
@@ -0,0 +1,32 @@
+#ifndef MENUSTATE_HPP
+#define MENUSTATE_HPP
+
+#include <vector>
+
+#include "button.hpp"
+#include "inputmanager.hpp"
+#include "renderer.hpp"
+#include "state.hpp"
+#include "texture.hpp"
+
+// The main menu of the game; It is the state shown when the game starts
+class MenuState: public State
+{
+public:
+    MenuState (InputManager *manager);
+    ~MenuState ();
+    void initialize () override;
+    void update () override;
+    void draw () override;
+    void exit () override;
+    void run () override;
+   
+    void addButton (Button *button);
+
+protected:
+    std::vector<Button*> mButtons;
+    Texture *title_text;
+    int index;                          // Shows the current button selected
+};
+
+#endif // MENUSTATE_HPP
